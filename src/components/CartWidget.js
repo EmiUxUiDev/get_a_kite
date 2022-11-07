2c8a0c67385fc7b4c9ffa6d47ff0acdfe1bde977
Emiliano <emilianoagu@hotmail.com>
2022-11-07 20:06:28 -0300

    PreEntrega1 Agu Emiliano - algunos retoques visuales en CSS

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,8 +1,16 @@
-import { MdOutlineShoppingCart } from "react-icons/md"
-import "../styles/cartwidget.css"
+import { MdOutlineShoppingCart } from "react-icons/md";
+import "../styles/cartwidget.css";
 
 export default function CartWidget() {
   return (
-    <div className="cart-icon"><MdOutlineShoppingCart /></div>
+    <div className="wrapper_cart">
+      <div className="cart-icon">
+        <MdOutlineShoppingCart />
+      </div>
+
+      <div className ='cart-item'>
+        <button>00</button>
+      </div>
+    </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,10 @@ export default function Navbar() {
   return (
     <header>
       <nav className="wrapper">
-        <img className="logo" src="logo125.png" alt="Getakite logo" />
+        <p className="wrapper_logo">GetAKite
+          <img className="logo" src="logo125.png" alt="Getakite logo" />
+        </p>
+
         <ul>
           <li>KITES</li>
           <li>BOARDS</li>
